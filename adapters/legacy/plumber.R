d2817a7fd0637a68c92706626b6c7e56a27787da
Akira Hayashi <rindrics@gmail.com>
2026-06-12 21:54:24 +0900

    chore: load data

--- adapters/legacy/plumber.R
+++ adapters/legacy/plumber.R
@@ -5,11 +5,18 @@ library(frasyr)
 #* @apiContact list(name = "Rindrics", url = "https://github.com/Rindrics/yoshimoto-samonji", email = "dev+yoshimoto-samonji@rindrics.com")
 #* @apiLicense list(name = "MIT", url = "https://opensource.org/licenses/MIT")
 #* @apiTag vpa Operations for Virtual Population Analysis
+#* @apiTag stock_assessment Operations for stock assessment
 
 #* Echo back the input
 #* @tag vpa
 #* @param msg The message to echo
 #* @get /vpa/echo
+data <- data.handler(
+    caa = read.csv("../../data/ex2_caa.csv", row.names = 1),
+    waa = read.csv("../../data/ex2_waa.csv", row.names = 1),
+    maa = read.csv("../../data/ex2_maa.csv", row.names = 1),
+    M = 0.5
+)
 function(msg="") {
   list(msg = paste0("The message is: '", msg, "'"))
 }
